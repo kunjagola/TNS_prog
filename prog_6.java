9a0fe60cd6e28463d20adfae995511169ba9ed41
kunjagola <105382567+kunjagola@users.noreply.github.com>
2023-05-30 13:13:59 +0530

    Student1

---
+++ prog_6.java
@@ -0,0 +1,19 @@
+package prog;
+
+public class Student1 {
+	private String name;
+	private int id;
+	private int age;
+	private int mobile;
+	
+	public void getData() {
+		name = "Kunj";
+		id = 1;
+		age = 20;
+		mobile = 94080;
+	}
+	
+	public void Display() {
+		System.out.println(name + " "+ id+ " "+ age+ " "+ mobile);
+	}
+}
